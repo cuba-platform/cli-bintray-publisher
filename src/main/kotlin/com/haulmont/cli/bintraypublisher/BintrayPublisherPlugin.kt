727caa2908dad2af4c0844c9eac71a95e4f203d5
Alexander Shustanov <shustanov@haulmont.com>
2019-02-15 10:01:57 +0400

    Updated to cli 2.0.4 and api version 4

--- src/main/kotlin/com/haulmont/cli/bintraypublisher/BintrayPublisherPlugin.kt
+++ src/main/kotlin/com/haulmont/cli/bintraypublisher/BintrayPublisherPlugin.kt
@@ -7,8 +7,7 @@ import com.haulmont.cuba.cli.ResourcesPath
 import com.haulmont.cuba.cli.event.InitPluginEvent
 
 class BintrayPublisherPlugin : CliPlugin {
-    override val apiVersion: Int
-        get() = 3
+    override val apiVersion = 4
 
     override val resources: ResourcesPath = HasResources("/com/haulmont/cli/bintraypublisher/")
 
